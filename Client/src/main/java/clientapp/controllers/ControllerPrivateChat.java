84d2b57f3955f1ca1f9577c5b3ff1c54b34db095
kotlaki <voland171@mail.ru>
2020-05-28 13:42:25 +0500

    Добавили private.fxml и ControllerPrivateChat.java

---
+++ Client/src/main/java/clientapp/controllers/ControllerPrivateChat.java
@@ -0,0 +1,21 @@
+package clientapp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class ControllerPrivateChat {
+    public TextArea txtPrivateChat;
+    public Button btnPrivateSend;
+    public Button btnPrivateExit;
+    public Button btnPrivateSendFile;
+
+    public void privateExit(ActionEvent actionEvent) {
+    }
+
+    public void privateSend(ActionEvent actionEvent) {
+    }
+
+    public void privateSendFile(ActionEvent actionEvent) {
+    }
+}
